41d7c57217e0d3e9cbcd49bfc03644a46e07f6e1
Danny Fong <danny.fong.dev@outlook.com>
2022-10-23 15:23:03 -0700

    mobile menu slides in and out

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 // UI Elements
-const menu = document.querySelector ('.mobile-menu-icon');
+const menuIcon = document.querySelector ('.mobile-menu-icon');
+const closeIcon = document.querySelector ('.close-menu-icon');
+const mobileMenu = document.querySelector('.mobile-nav');
+const overlay = document.querySelector('.overlay');
 
 // Load Event Listener
-menu.addEventListener ('click', handleClickMenu);
+menuIcon.addEventListener ('click', handleClickMenu);
+closeIcon.addEventListener ('click', handleClickCloseMenu);
 
 // UI Handlers
 function handleClickMenu(e) {
-  console.log (menu);
+  // overlay.style.wdith = '100vw';
+  // overlay.style.length = '100vh';
+  // overlay.style.background = "#000";
+  closeIcon.style.display = 'block';
+  closeIcon.style.zIndex = 10;
+  menuIcon.style.display = "none";
+  showMobileMenu();
+}
+
+function handleClickCloseMenu(e) {
+  closeIcon.style.display = 'none';
+  menuIcon.style.display = "block";
+  closeMobileMenu();
+}
+
+function showMobileMenu() {
+  console.log(mobileMenu);
+  mobileMenu.style.display = 'block'
+  mobileMenu.style.width = '70%';
+}
+
+function closeMobileMenu() {
+  // mobileMenu.style.display = "none";
+  mobileMenu.style.width = '0%';
 }
