49cda94b2c9926b68c35fc6fc437d8a78552cc95
Danny Fong <danny.fong.dev@outlook.com>
2022-10-23 12:39:40 -0700

    mobile menu js stub created

---
+++ app.js
@@ -0,0 +1,10 @@
+// UI Elements
+const menu = document.querySelector ('.mobile-menu-icon');
+
+// Load Event Listener
+menu.addEventListener ('click', handleClickMenu);
+
+// UI Handlers
+function handleClickMenu(e) {
+  console.log (menu);
+}
