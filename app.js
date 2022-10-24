ab7f4ad98064e11ec527b028d07ccf50ffc54f47
Danny Fong <danny.fong.dev@outlook.com>
2022-10-23 22:33:45 -0700

    layout tweaks for mobile version

--- app.js
+++ app.js
@@ -1,44 +1,19 @@
 // UI Elements
-const menuIcon = document.querySelector ('.mobile-menu-icon');
-const closeIcon = document.querySelector ('.close-menu-icon');
-const mobileMenu = document.querySelector('.mobile-nav');
+const menuIcon = document.querySelector('.mobile-menu-icon');
+const closeIcon = document.querySelector('.close-menu-icon');
+const mobileNav = document.querySelector('.mobile-nav');
 const menuList = document.querySelector('.menu-list');
 
 // Load Event Listener
-menuIcon.addEventListener ('click', handleClickMenu);
-closeIcon.addEventListener ('click', handleClickCloseMenu);
+menuIcon.addEventListener('click', handleOpenMenu);
+closeIcon.addEventListener('click', handleCloseMenu);
 
 // UI Handlers
-function handleClickMenu(e) {
-  closeIcon.style.display = 'block';
-  closeIcon.style.zIndex = 10;
-  menuIcon.style.display = "none";
-  showMobileMenu();
+function handleOpenMenu(e) {
+  // console.log(e)
+  mobileNav.style.display = 'block';
 }
 
-function handleClickCloseMenu(e) {
-  closeIcon.style.display = 'none';
-  menuIcon.style.display = 'block';
-  closeMobileMenu();
+function handleCloseMenu(e) {
+  mobileNav.style.display = 'none';
 }
-
-function showMobileMenu() {
-  mobileMenu.style.transition = '1s';
-  mobileMenu.style.display = 'block'
-  mobileMenu.style.width = '100%';
-  mobileMenu.style.height = '100%';
-  mobileMenu.style.zIndex = 9;
-  mobileMenu.style.transition = '1s';
-  menuList.style.display = 'block';
-  menuList.style.width = '70%';
-  menuList.style.height = '100%';
-  
-}
-
-function closeMobileMenu() {
-  mobileMenu.style.transition = '1s';
-  mobileMenu.style.display = 'none'
-  mobileMenu.style.width = '0%';
-  mobileMenu.style.transition = '1s';
-  menuList.style.display = 'none';
-  menuList.style.width = '0%';}
